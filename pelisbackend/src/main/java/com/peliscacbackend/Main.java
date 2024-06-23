c7c9ec08cfe66d4ca3245b1116df7f4c07ed07dd
Pablo <pbasualdoarcati@hotmail.com>
2024-06-23 18:32:59 -0300

    first commit

---
+++ pelisbackend/src/main/java/com/peliscacbackend/Main.java
@@ -0,0 +1,33 @@
+package com.peliscacbackend;
+
+import java.sql.*;
+import java.sql.SQLException;
+import java.sql.DriverManager;
+
+public class Main {
+
+    private Connection conectar = null;
+    private final String usuario = "VA EL USUARIO DE CADA UNO";
+    private final String contraseña = "VA LA CONTRASEÑA DE CADA UNO";
+    private final String db = "proyecto_movies_2024";
+    private final String ip = "localhost"; 
+    private final String puerto = "3306";//Chequeen su puerto
+    private final String cadena = "jdbc:mysql://" + ip + ":" + puerto + "/" + db;
+
+
+    public Connection establecerConexion() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conectar = DriverManager.getConnection(cadena, usuario, contraseña);
+            System.out.println("Conexion exitosa");
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println("Error al conectar " + e.getMessage());
+        }
+        return conectar;
+    }
+    public static void main(String[] args) {
+        Main app = new Main();
+        app.establecerConexion();
+
+    }
+}
---
+++ pelisbackend/src/main/java/com/peliscacbackend/Pelicula.java
@@ -0,0 +1,94 @@
+package com.peliscacbackend;
+
+public class Pelicula {
+
+
+    private Integer idPelicula; /*privado es el tipo de encapsulamiento */
+    private String titulo;
+    private String duracion;
+    private String imagen;
+    private String synopsis;
+    private Integer idActor;
+    private Integer idDirector;
+    private Integer idGenero;   
+
+    //metodo CONSTRUCTOR
+    public Pelicula(Integer idPelicula, String titulo, String duracion, String imagen, String synopsis, Integer idActor, Integer idDirector, Integer idGenero){
+
+        this.idPelicula = idPelicula;
+        this.titulo = titulo;
+        this.duracion = duracion;
+        this.imagen = imagen;
+        this.synopsis = synopsis;
+        this.idActor=idActor;
+        this.idDirector=idDirector;
+        this.idGenero=idGenero;
+
+    }
+
+    public Integer getIdPelicula() {
+        return idPelicula;
+    }
+
+    public void setIdPelicula(Integer idPelicula) {
+        this.idPelicula = idPelicula;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(String duracion) {
+        this.duracion = duracion;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+
+    public String getSynopsis() {
+        return synopsis;
+    }
+
+    public void setSynopsis(String synopsis) {
+        this.synopsis = synopsis;
+    }
+
+    public Integer getIdActor() {
+        return idActor;
+    }
+
+    public void setIdActor(Integer idActor) {
+        this.idActor = idActor;
+    }
+
+    public Integer getIdDirector() {
+        return idDirector;
+    }
+
+    public void setIdDirector(Integer idDirector) {
+        this.idDirector = idDirector;
+    }
+
+    public Integer getIdGenero() {
+        return idGenero;
+    }
+
+    public void setIdGenero(Integer idGenero) {
+        this.idGenero = idGenero;
+    }
+    
+
+}
