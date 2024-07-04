aeeafbcad72b8d7ced1fb418ba64429873d46130
Maximiliano Minutillo <mminutillo@tecnoazar.com.ar>
2024-07-04 10:12:26 -0300

    adding controller

---
+++ pelisbackend/src/main/java/com/peliscacbackend/Controlador.java
@@ -0,0 +1,15 @@
+package com.peliscacbackend;
+
+
+// Importaciones necesarias para la clase Controlador
+import javax.servlet.annotation.WebServlet; // Importación de WebServlet para la anotación que mapea este servlet a una URL específica
+import javax.servlet.http.HttpServlet; // Importación de HttpServlet para extender esta clase y manejar peticiones HTTP
+
+
+// Clase Controlador: Maneja las peticiones HTTP para insertar y recuperar películas.
+@WebServlet("/peliculas") // Anotación que mapea este servlet a la URL "/peliculas"
+public class Controlador extends HttpServlet { // Declaración de la clase Controlador que extiende HttpServlet
+ 
+    // Método POST para insertar una nueva película desde una solicitud JSON
+ 
+}
